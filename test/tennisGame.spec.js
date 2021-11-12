0972abb59972e53b7dc89678a71aec4732a6524d
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-12 17:29:07 +0000

    tdd - add the first test to make sure starting score is love-all

--- test/tennisGame.spec.js
+++ test/tennisGame.spec.js
@@ -1,7 +1,14 @@
+class TenisGame {
+  constructor(score) {
+    this.score = score;
+  }
+}
+
 describe('Tennis Game Kata', () => {
-  describe('test', () => {
-    test('will pass', () => {
-      expect(2 + 2).toBe(4);
+  describe('When the game starts', () => {
+    test('the score should be love-all', () => {
+      const game = new TenisGame('love-all');
+      expect(game.score).toBe('love-all');
     });
   });
 });
