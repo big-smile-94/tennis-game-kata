cc506fd6c1f747e6b4373c970e46d16125811005
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-12 22:46:55 +0000

    refactor the first test

---
+++ src/TennisGame.js
@@ -0,0 +1,5 @@
+export default class TennisGame {
+  constructor(score) {
+    this.score = score;
+  }
+}
--- test/tennisGame.test.js
+++ test/tennisGame.test.js
@@ -1,13 +1,10 @@
-class TenisGame {
-  constructor(score) {
-    this.score = score;
-  }
-}
+import TennisGame from '../src/TennisGame';
 
 describe('Tennis Game Kata', () => {
   describe('When the game starts', () => {
     test('the score should be love-all', () => {
-      const game = new TenisGame('love-all');
+      console.log('Some log');
+      const game = new TennisGame('love-all');
       expect(game.score).toBe('love-all');
     });
   });
