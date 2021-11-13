f33db1c73d4f96f510eb131a5dc14e6d4415c70e
Hassan Akbar <hassan.akbar@zeninternet.co.uk>
2021-11-13 00:19:17 +0000

    refactor - add a gameState mapper

--- src/TennisGame.js
+++ src/TennisGame.js
@@ -4,10 +4,20 @@ export default class TennisGame {
   }
 
   player1Scores() {
-    return new TennisGame('15-love');
+    return gameState.find((g) => g.score === this.score).nextState.player1;
   }
 
   player2Scores() {
-    return new TennisGame('love-15');
+    return gameState.find((g) => g.score === this.score).nextState.player2;
   }
 }
+
+const gameState = [
+  {
+    score: 'love-all',
+    nextState: {
+      player1: new TennisGame('15-love'),
+      player2: new TennisGame('love-15'),
+    },
+  },
+];
--- test/tennisGame.test.js
+++ test/tennisGame.test.js
@@ -4,18 +4,17 @@ describe('Tennis Game Kata', () => {
   //TODO: remove the second describe!
   describe('When the game starts', () => {
     test('the score should be love-all', () => {
-      console.log('Some log');
       const game = new TennisGame('love-all');
       expect(game.score).toBe('love-all');
     });
 
     test('if player one scores the score should be 15-love', () => {
-      const tennis = new TennisGame('love-love').player1Scores();
+      const tennis = new TennisGame('love-all').player1Scores();
       expect(tennis.score).toBe('15-love');
     });
 
     test('if player two scores the score should be love-15', () => {
-      const tennis = new TennisGame('love-love').player2Scores();
+      const tennis = new TennisGame('love-all').player2Scores();
       expect(tennis.score).toBe('love-15');
     });
   });
